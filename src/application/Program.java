fda7f3f3aa76ba0cd70da320ca9aa34f1451adcc
devPoa-Java <sandromarista@gmail.com>
2025-07-04 06:10:56 -0300

    Update and delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,11 +30,23 @@ public class Program {
 		for(Seller obj : listall) {
 			System.out.println(obj);
 		}
-		System.out.println("\n=== Teste 4: Seller Insert ===");
 		
+		System.out.println("\n=== Teste 4: Seller Insert ===");
 		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id= " + newSeller.getId());
+		
+		System.out.println("\n=== Teste 5: Seller Update ===");
+	    seller = sellerDao.findById(1);
+	    seller.setName("Sandro M. dos Santos");
+	    sellerDao.update(seller);
+	    System.out.println("---Update completed!---");
+	    
+	    System.out.println("\n=== Teste 6: Seller Delete ===");
+	    sellerDao.deleteById(10);
+	    System.out.println("---Delete completed!---");
+	    
+	    
 	}
 
 }
